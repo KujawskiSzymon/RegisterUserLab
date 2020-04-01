a0dbea002378177b2fdd55cefa4a91b7f79cf5a8
KujawskiSzymon <42217160+KujawskiSzymon@users.noreply.github.com>
2020-04-01 16:46:14 +0200

    HashUserRole

---
+++ RegisterUserLab/Models/HashUserRole.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RegisterUserLab.Models
+{
+    public class HashUserRole
+    {
+        [Key]
+        public  int Id { get; set; }
+        [Required]
+        public int UserId { get; set; }
+        [Required]
+        public int RoleId { get; set; }
+    }
+}
