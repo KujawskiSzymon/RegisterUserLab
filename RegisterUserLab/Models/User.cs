c84f59b37e9fdeaea09b293c5c25e3bec0ad6e25
KujawskiSzymon <42217160+KujawskiSzymon@users.noreply.github.com>
2020-04-01 16:40:32 +0200

    User

--- RegisterUserLab/Models/User.cs
+++ RegisterUserLab/Models/User.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +8,23 @@ namespace RegisterUserLab.Models
 {
     public class User
     {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Nazwisko { get; set; }
+
+        public string Stanowisko{ get; set; } 
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        public string Login { get; set; }
+        [Required]
+        public string Haslo { get; set; }
+        [Required]
+        public bool isMale { get; set; }
+        public bool isAccountActive { get; set; } = true;
+        public DateTime RegisterDate { get; set; } = DateTime.Now;
     }
 }
