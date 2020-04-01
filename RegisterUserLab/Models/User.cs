a3342c19bee2181078cfa2a7e9bc9e2f83bbd1ae
KujawskiSzymon <42217160+KujawskiSzymon@users.noreply.github.com>
2020-04-01 17:01:39 +0200

    User fix names, front in work

--- RegisterUserLab/Models/User.cs
+++ RegisterUserLab/Models/User.cs
@@ -13,15 +13,15 @@ namespace RegisterUserLab.Models
         [Required]
         public string Name { get; set; }
         [Required]
-        public string Nazwisko { get; set; }
+        public string Surname { get; set; }
 
-        public string Stanowisko{ get; set; } 
+        public string Position{ get; set; } 
         [Required]
         public string Email { get; set; }
         [Required]
         public string Login { get; set; }
         [Required]
-        public string Haslo { get; set; }
+        public string Password { get; set; }
         [Required]
         public bool isMale { get; set; }
         public bool isAccountActive { get; set; } = true;
